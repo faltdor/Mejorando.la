466c4b171876216fce9e65c65d0ea23deb91ef6e
Fenixsoftwarecolombia <Fenixsoftwarecolombia@gmail.com>
2015-04-05 13:56:33 -0500

    Actualizzacion de Recursos Clase 4

---
+++ CURSO_DE_DISENO_WEB_ONLINE/clase1/formulario.js
@@ -0,0 +1,32 @@
+var $form = $(".formulario"),
+	$titulo = $("#titulo"),
+	$url= $("#url"),
+	$button = $("#mostrar_form"),
+	$list = $("#contenido"),
+	$post = $(".item").first();
+
+
+function mostrarFormulario(){
+	$form.slideToggle();
+	return false;
+}
+function agregarPos(){
+	var url = $url.val(),
+		titulo = $titulo.val();
+	
+	$clone = $post.clone();
+
+		$clone.find('.titulo_item a')
+			  .text(titulo)
+			  .attr('href',url);
+		$clone.hide();
+	$list.prepend($clone);
+		$clone.fadeIn();
+
+	return false;
+}
+
+$button.click(mostrarFormulario);
+
+$form.on('submit',agregarPos);
+
